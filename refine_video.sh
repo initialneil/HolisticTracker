141255c806c520c13b8a6ea1379dabd4980a43ea
szj <initialneil@gmail.com>
2025-11-25 17:52:46 +0000

    - refine ubc fashion

--- refine_video.sh
+++ refine_video.sh
@@ -6,11 +6,37 @@
 #     --optim_cfg "src/configs/optim_configs/ehm_fbody_scan.yaml" \
 #     --not_check_hand
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3 \
+#     accelerate launch --main_process_port 29070 \
+#     refine_video.py \
+#     --in_root "/home/szj/err_empty_syn/ytb/bbox_square_x1.1/dataset_ehm_v1" \
+#     --output_dir "/home/szj/err_empty_syn/ytb/bbox_square_x1.1/dataset_ehm_v2.1" \
+#     --optim_cfg "src/configs/optim_configs/ehm_fbody_ytb.yaml" \
+#     --not_check_hand \
+
+# CUDA_VISIBLE_DEVICES=3 \
+#     accelerate launch --main_process_port 29071 \
+#     refine_video.py \
+#     --in_root "/home/szj/err_empty_syn/ytb/bbox_square_x1.1/dataset_ehm_v1" \
+#     --output_dir "/home/szj/err_empty_syn/ytb/bbox_square_x1.1/dataset_ehm_v2.1" \
+#     --optim_cfg "src/configs/optim_configs/ehm_fbody_ytb.yaml" \
+#     --not_check_hand \
+#     --reversed_order \
+
+CUDA_VISIBLE_DEVICES=1 \
     accelerate launch --main_process_port 29070 \
-    --in_root "/home/szj/err_empty_syn/ytb/bbox_square_x1.1/dataset_ehm_v1" \
-    --output_dir "/home/szj/err_empty_syn/ytb/bbox_square_x1.1/dataset_ehm_v2" \
+    refine_video.py \
+    --in_root "/home/szj/err_empty_syn/fashion/train_10fps" \
+    --output_dir "/home/szj/err_empty_syn/fashion/train_10fps_v2" \
     --optim_cfg "src/configs/optim_configs/ehm_fbody_ytb.yaml" \
     --not_check_hand \
-    # --reversed_order \
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3 \
+#     accelerate launch --main_process_port 29070 \
+#     refine_video.py \
+#     --in_root "/home/szj/err_empty_syn/fashion/test_30fps" \
+#     --output_dir "/home/szj/err_empty_syn/fashion/test_30fps_v2" \
+#     --optim_cfg "src/configs/optim_configs/ehm_fbody_ytb.yaml" \
+#     --not_check_hand \
 
